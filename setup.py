b4869a29dd64f7c404a7b9c619604bc672ee2276
Pramod1818 <pk1818202@gmail.com>
2024-06-27 01:05:10 +0530

    e has added

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+# from typing import List
+
+# def get_requirements()->list[str]:
+#     requirements_list = list[str] = []
+
+#     return requirements_list
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="pramod",
+    author_email="pk1818202@gmail.com",
+    packages=find_packages(),
+    install_requires =   ['pymongo'] #get_requirements(),
+)
