711fd642169b09431ca10161b5526bb37bdbbc45
Pramod1818 <pk1818202@gmail.com>
2024-06-30 16:29:43 +0530

    Data Ingestion and Data Validation Modules

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from sensor.exception import SensorException
 import os,sys
 from sensor.logger import logging
+# from sensor.utils import dump_csv_file_to_mongodb_collection
+from sensor.pipeline.training_pipeline import TrainPipeline
 
-def test():
-    try:
-        logging.info("Error due to divide by zero")
-        a=3/0
-    except Exception as e:
-        # raise e
-        raise SensorException(e,sys)    
+# def test():
+#     try:
+#         logging.info("Error due to divide by zero")
+#         a=3/0
+#     except Exception as e:
+#         # raise e
+#         raise SensorException(e,sys)    
 
 if __name__ =="__main__":
-    try:
-        test()
-    except Exception as e:
-        print(e)
+    # file_path = r'C:\Users\Pramodkumar\Desktop\Project_All\ML2\sensor\aps_failure_training_set1.csv'
+    # database_name="sensordb"
+    # collection_name= "sensor"
+    # dump_csv_file_to_mongodb_collection(file_path,database_name,collection_name)
+
+    training_pipeline = TrainPipeline()
+    training_pipeline.run_pipeline()
+
+
+    # try:
+    #     test()
+    # except Exception as e:
+    #     print(e)
---
+++ sensor/__init__.py
@@ -0,0 +1,3 @@
+from dotenv import load_dotenv
+print(f"for reading the dotenv file")
+load_dotenv()
